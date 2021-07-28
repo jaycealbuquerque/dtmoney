be5e88b1be1a8c11a9712f02fc3be2fd58c759a3
“jeycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2021-07-28 18:08:54 -0300

    cria modal  para o botão Nova Transação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./style/global";
 
+Modal.setAppElement('#root')
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleOpenNewTransactionClose(){
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleOpenNewTransactionClose}
+      >
+      
+        <h2>Cadastra transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
