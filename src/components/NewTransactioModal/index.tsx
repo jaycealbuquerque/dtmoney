5d4a55857cf36bcc611f14d13494a2aab34df5f7
“jeycealbuquerque” <jeycealbuquerquerocha@gmail.com>
2021-07-29 06:18:09 -0300

    cria botoes de tipo de transação

--- src/components/NewTransactioModal/index.tsx
+++ src/components/NewTransactioModal/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer, RadionBox } from './styles';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -8,6 +11,10 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal ({ isOpen, onRequestClose}: NewTransactionModalProps){
+  const [type, setType] = useState('deposit');
+
+  
+
   return (
     <Modal 
         isOpen={isOpen} 
@@ -27,6 +34,29 @@ export function NewTransactionModal ({ isOpen, onRequestClose}: NewTransactionMo
           <h2>Cadastra transação</h2>
           <input placeholder="Título" />
           <input type="number" placeholder="Valor" />
+
+          <TransactionTypeContainer>
+            <RadionBox
+              type="button"
+              onClick={() => { setType('deposit');}}
+              isActive={ type === 'deposit'}
+              activeColor="green"
+            >
+              <img src={incomeImg} alt="Entrada" />
+              <span>Entrada</span>
+            </RadionBox>
+
+            <RadionBox
+              type="button"
+              onClick={() => { setType('withdraw');}}
+              isActive={ type === 'withdraw'}
+              activeColor="red"
+            >
+              <img src={outcomeImg} alt="Saida" />
+              <span>Saida</span>
+            </RadionBox>
+          </TransactionTypeContainer>
+
           <input placeholder="Categoria" />
           <button type="submit">Cadastrar  </button>
         </Container>
