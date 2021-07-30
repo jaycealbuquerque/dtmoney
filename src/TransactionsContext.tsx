0bc7a32fe2d0970223ff904a7f4a3a4f3e8c18bb
jaycealbuquerque <jeycealbuquerquerocha@gmail.com>
2021-07-30 20:13:49 -0300

    finaliza a inserção de uma nova transação

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -18,7 +18,7 @@ interface TransactionsProviderProps {
 
 interface TransactionsContextData {
   transactions: Transaction[];
-  createTransaction: (transaction: TransactionInput) => void;
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
 }
 
 export const TransactionsContext = createContext<TransactionsContextData>(
@@ -33,8 +33,18 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       .then(response => setTransactions(response.data.transactions))
   },[]);
 
-  function createTransaction(transaction: TransactionInput) {
-    api.post('/transaction', transaction )
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response =  await api.post('/transaction', {
+      ...transactionInput,
+      createdAt: new Date(),
+    
+    })
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+       transaction
+    ]); 
   }
 
   return (
